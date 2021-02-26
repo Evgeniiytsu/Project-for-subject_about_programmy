7d6e1d0fbfefcbbd8656ce4a2943994ca6c437b0
Evgeniiytsu <77857954+Evgeniiytsu@users.noreply.github.com>
2021-02-26 11:28:51 +0300

    TASK-1-CALCULATOR
    TASK-1-CALCULATOR

--- task-1-calculator/Program (full version).cs
+++ task-1-calculator/Program (full version).cs
@@ -15,68 +15,123 @@ namespace task_1_calculator
             double total;
             char oper;
             char again;
-            while ("true")
-                
-           
-            Console.WriteLine("Введите первое число."); //ПРОВЕСТИ КОНВЕРТИРОВАНИЕ
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите оператор.");
-            oper = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Введите второе число.");
-            b = Convert.ToDouble(Console.ReadLine());
 
+            while (true)
 
-            if (oper == '+')
 
             {
 
-                total = a+b;
-                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+                Console.WriteLine("Введите первое число."); //ПРОВЕСТИ КОНВЕРТИРОВАНИЕ
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите оператор.");
+                oper = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Введите второе число.");
+                b = Convert.ToDouble(Console.ReadLine());
+
+
+                if (oper == '^')
+
+                {
+                    total = a * a;
+                    Console.WriteLine("Квадрат" + a + "равна" + total + ".");
+
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Введите второе число");
+                    a = Convert.ToDouble(Console.ReadLine());
+
+                    if (oper == '+')
+
+                    {
+
+                        total = a + b;
+                        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+
+                    }
+                    if (oper == '-')
+                    {
+                        total = a - b;
+
+                        Console.WriteLine("Разность" + a + " и " + b + " равна " + total + ".");
+                    }
+
+
+                    if (oper == '*')
+                    {
+                        total = a * b;
+
+                        Console.WriteLine("Произведение" + a + "и" + b + " равна " + total + ".");
+                    }
+                    if (oper == '/')
+                    {
+                        total = a / b;
+
+                        Console.WriteLine("Деление" + a + "и" + b + " равна " + total + ".");
+                    }
+
+
+
+
+
+
+
+
+
+
+                    if (oper == '+')
+
+                    {
+
+                        total = a + b;
+                        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+
+                    }
+
+                    if (oper == '-')
+                    {
+
+                        total = a - b;
+
+                        Console.WriteLine("Разность" + a + " и " + b + " равна " + total + ".");
+                    }
+
+                    if (oper == '*')
+
+                    {
+                        total = a * b;
+
+                        Console.WriteLine("Произведение" + a + "и" + b + " равна " + total + ".");
+
+                    }
+
+
+                    if (oper == '/')
+
+                    {
+
+                        total = a / b;
+
+                        Console.WriteLine("Деление" + a + "и" + b + " равна " + total + ".");
+
+                    }
+
+
+                    else
+                    {
+
+
+                        Console.WriteLine("Неизвестный оператор.");
+                    }
+
+
+
+
+                }
 
             }
-
-            if (oper == '-') 
-            {
-
-                total = a-b;
-
-                Console.WriteLine("Разность" + a + " и " + b + " равна " + total + ".");
-            }
-
-            if (oper == '*') 
-
-            {
-                total = a*b;
-
-                Console.WriteLine("Произведение" + a + "и" + b + " равна " + total + ".");
-
-            }
-
-
-            if (oper == '/') 
-
-            {
-
-                total = a / b;
-
-                Console.WriteLine("Деление" + a + "и" + b + " равна " + total + ".");
-
-            }
-
-
-            else
-            {
-
-
-                Console.WriteLine("Неизвестный оператор.");
-            }
-
-            
-
-                
-                
-
-
             
 
 
